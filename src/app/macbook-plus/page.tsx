fe841514de789fbe4da5700f016039c153f83143
syedfarhandesigns <syedfarhandesigns@gmail.com>
2024-07-01 17:19:21 +0000

    First commit on idx

---
+++ src/app/macbook-plus/page.tsx
@@ -0,0 +1,15 @@
+import SubNav from '@/components/ui/subNav'
+import React from 'react'
+
+function page() {
+  return (
+    <main className='' >
+        <SubNav title='Macbook Plus' link='/' />
+        <section className='pt-36 px-10' >
+            <h1 className='heading gradientText text-center' >Macbook Plus</h1>
+        </section>
+    </main>
+  )
+}
+
+export default page
