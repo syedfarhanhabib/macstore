141ca5ea86fc798637f9ed1fc61cf1dee4048559
syedfarhandesigns <syedfarhandesigns@gmail.com>
2024-07-02 16:00:30 +0000

    Updated styling

--- src/app/macbook-plus/page.tsx
+++ src/app/macbook-plus/page.tsx
@@ -1,42 +1,44 @@
 'use client';
-import SubNav from '@/components/ui/subNav'
-import React from 'react'
 
-function page() {
-  const [scrolled, setScrolled] = React.useState(false);
+import SubNav from '@/components/ui/subNav';
+import React from 'react';
+
+const Page: React.FC = () => {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    if (scrollY > 0 && videoRef.current) {
+      videoRef.current.play();
+    } else if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
 
   React.useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
     window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+
   return (
-    <main className='h-[200vh]' >
+    <main className='h-[200vh]'>
       <SubNav title='Macbook Plus' link='/' />
-      <section className='pt-36 overflow-x-hidden' >
-        <h3 className='title text-center' >Macbook Plus</h3>
-        <h1 className='text-center heading gradientText' >
+      <section className='pt-36 overflow-x-hidden'>
+        <h3 className='title text-center'>Macbook Plus</h3>
+        <h1 className='text-center heading gradientText'>
           Mind-blowing.
-          Head-turning.<br />
-          Unrivalled.</h1>
+          <br />
+          Unrivalled.
+        </h1>
         <video
           muted
-          // autoPlay
-          autoPlay={scrolled}
-          className='w-[5990px] object-contain'
-          src="https://www.apple.com/105/media/us/macbook-pro/2023/232a2dbf-5898-4fd1-a350-6a7c5c2e31c9/anim/welcome-hero/large.mp4"></video>
+          ref={videoRef}
+          className='w-[130vw] absolute center translate-y-20 object-contain'
+          src='https://www.apple.com/105/media/us/macbook-pro/2023/232a2dbf-5898-4fd1-a350-6a7c5c2e31c9/anim/welcome-hero/large.mp4'
+        ></video>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
